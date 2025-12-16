6c835fb3a7ba63ea486752240cdd08f311eaa38d
Ibrahima CAMARA <camsoib01@gmail.com>
2025-12-16 11:40:10 +0000

    Deploy new portfolio build added new files

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/github.io/', // ✅ correct pour ton repo actuel
+  // base: '/github.io/', // ✅ correct pour ton repo actuel
+  base: '/',
 });
 
